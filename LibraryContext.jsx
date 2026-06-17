f67f7f2fb8cf9d6d8c9fd1ae1ca0079ada9cd53d
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2026-06-18 00:51:19 +0200

    Update LibraryContext.jsx

--- LibraryContext.jsx
+++ LibraryContext.jsx
@@ -16,7 +16,7 @@ export const LibraryProvider = ({ children }) => {
         const initialBooks = [
           { id: '1', title: 'Title01', slug: 'title01', author: 'Author01', genre: 'Классика', year: 2000, pages: 328 },
           { id: '2', title: 'Title02', slug: 'title02', author: 'Author02', genre: 'Фентези', year: 2001, pages: 310 },
-          { id: '3', title: 'Title03', slug: 'title03', author: 'Author03', genre: 'Фантастика', year: 20002, pages: 480 }
+          { id: '3', title: 'Title03', slug: 'title03', author: 'Author03', genre: 'Фантастика', year: 2002, pages: 480 }
         ];
         setBooks(initialBooks);
         setIsLoading(false);
